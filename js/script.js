59e71e9e2b7fc32c762d6dc13834f6834be4bab1
filippemonteiro <filippemonteiro@outlook.com>
2025-04-25 14:40:46 -0300

    remove espaços em branco desnecessários no script de cálculo de IMC

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 const button = document.getElementById("button"),
   pesoInput = document.getElementById("peso"),
   alturaInput = document.getElementById("altura");
-
 button.addEventListener("click", function (event) {
   event.preventDefault();
   const peso = parseFloat(pesoInput.value),
     altura = parseFloat(alturaInput.value),
     imc = peso / (altura * altura);
-
-  alert(`Seu IMC é ${imc.toFixed(2)} kg/m².`);
-});
+  alert(`Seu IMC é ${imc.toFixed(2)} kg/m².`);});
